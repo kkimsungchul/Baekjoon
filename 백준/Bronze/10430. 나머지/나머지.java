5485bb3bfc3ae676e17d3f6571319f81b8a46c10
kimsungchul <82440779+kkimsungchul@users.noreply.github.com>
2022-10-26 17:48:47 +0900

    [Bronze V] Title: 나머지, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args){
+        //첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+        
+        
+    }
+}
