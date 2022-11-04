3ab994a15be8d3be8159cb38c49bd868e8cf8262
kimsungchul <82440779+kkimsungchul@users.noreply.github.com>
2022-11-04 10:27:22 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.09 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long sum = 0;
+
+        for(int i=1; i<=count; i++){
+            sum = sum + price*i;
+        }
+
+
+        if(sum>money){
+            answer = sum - money;
+            return answer;
+        }
+        else{
+            return 0;
+        }
+        
+    }
+}
