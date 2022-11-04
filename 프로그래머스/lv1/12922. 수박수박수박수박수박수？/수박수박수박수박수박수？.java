db1191f4f7855e6fe4fecdccd912c6949f55bf81
kimsungchul <82440779+kkimsungchul@users.noreply.github.com>
2022-11-04 10:25:04 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.39 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        String arr[] = {"수","박"};
+        
+        for(int i=0;i<n;i++){
+            answer+=arr[i%2];
+        }
+        
+        return answer;
+    }
+}
