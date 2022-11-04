98488b5ff3b9f05b2ff16d6e68c9cb5fb89cebf2
kimsungchul <82440779+kkimsungchul@users.noreply.github.com>
2022-11-04 10:31:29 +0900

    [level 1] Title: 모의고사, Time: 0.57 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+        int one[] = {1, 2, 3, 4, 5}; //5
+        int two[] = {2, 1, 2, 3, 2, 4, 2, 5}; //8
+        int three[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //10
+        int count[] ={0,0,0};
+
+        boolean success = false;
+        
+        for(int i=0;i<answers.length;i++){
+            if(answers[i]==one[i%5]){
+                count[0]++;
+            }
+            if(answers[i]==two[i%8]){
+                count[1]++;
+            }
+            if(answers[i]==three[i%10]){
+                count[2]++;
+            }
+        }
+        int max = Math.max(count[0], Math.max(count[1], count[2]));
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for(int i=0;i<count.length;i++){
+            if(count[i]==max){
+                 arrayList.add(i+1);
+            }
+        }
+        int[] answer = new int[arrayList.size()];
+        int cnt = 0;
+        for(int num : arrayList)
+            answer[cnt++] = num;
+        return answer;
+    }
+}
