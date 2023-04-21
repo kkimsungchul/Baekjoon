27ebd4f1eff7823c9e10d82645803242719cbec6
kimsungchul <82440779+kkimsungchul@users.noreply.github.com>
2023-04-21 20:11:15 +0900

    [level 2] Title: 의상, Time: 0.32 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        // 의상의 종류별로 갯수를 세어서 Map에 저장합니다.
+        for (String[] c : clothes) {
+            String type = c[1];
+            if (!map.containsKey(type)) {
+                map.put(type, 1);
+            } else {
+                map.put(type, map.get(type) + 1);
+            }
+        }
+        
+        // 각 의상 종류별로 입을지 입지 않을지 선택하는 경우의 수를 곱하여 결과를 구합니다.
+        for (int value : map.values()) {
+            answer *= (value + 1);
+        }
+        
+        // 아무것도 입지 않은 경우는 제외하므로 1을 빼고 결과를 반환합니다.
+        return answer - 1;
+    }
+}
