ed6b5f2829648420bc51db335385d4a9df364a73
kimsungchul <82440779+kkimsungchul@users.noreply.github.com>
2023-04-22 15:42:11 +0900

    [level 2] Title: 주식가격, Time: 17.09 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,30 +1,23 @@
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        int intArr[] = new int[prices.length];
-        int count=0;
-        boolean pass =false;
-        for(int i=0;i<prices.length-1;i++){
+        int count =0;
+        for(int i=0;i<prices.length;i++){
             for(int j=i+1;j<prices.length;j++){
-                count++;
                 if(prices[i]>prices[j]){
-                    intArr[i]=count;
-                    count=0;
-                    pass=true;
+                    count++;
                     break;
+                }else{
+                    count++;
                 }
             }
-            //System.out.println("### i : " + i + " pass : " + pass);
-            if(!pass){
-                
-                intArr[i]=count;
-                count=0;    
-            }else{
-                pass=false;
-            }
+            answer[i]=count;
+            count=0;
+            
             
         }
         
-        return intArr;
+        
+        return answer;
     }
 }
